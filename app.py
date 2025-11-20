537a261c3cceb6e389660be1d29efd6eefe8e45f
00126222-dev <00126222@uca.edu.sv>
2025-11-19 20:49:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st
-from modulos.bienvenido import mostrar_bienvenido  
+from modulos.bienvenido import mostrar_bienvenido # Importamos la función mostrar_bienvenido del módulo bienvenido
+
+# Llamamos a la función mostrar_bienvenido para mostrar el mensaje en la app
 mostrar_bienvenido()
