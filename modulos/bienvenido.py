057cc5dc3e080312f51e49776b33b050daaf6950
00126222-dev <00126222@uca.edu.sv>
2025-11-19 20:42:16 -0600

    Update bienvenido.py

--- modulos/bienvenido.py
+++ modulos/bienvenido.py
@@ -1,5 +1,5 @@
 import streamlit as st
-def mostrar_venta():
+def mostrar_bienvenido():
  # Aquí se muestra "Hola Mundo"
  st.title("Bienvenidos al sistema de los Grupos de Ahorro y Préstamo Comunitario (GAPC) ")
  st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
