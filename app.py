8ea504734404353ca53af88b691b31a8b4e3fdf9
00126222-dev <00126222@uca.edu.sv>
2025-11-20 16:15:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 
 import streamlit as st
 from modulos.bienvenido import mostrar_bienvenido # Importamos la función mostrar_bienvenido del módulo bienvenido
-mostrar_bienvenido()
+
 from modulos.login import login
 
 # Comprobamos si la sesión ya está iniciada
 # Esta línea verifica si la clave "sesion_iniciada" existe en el estado de la sesión
 # Y también comprueba si su valor es True.
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
-    
+    opciones = ["Ventas", "Otra opción"] # Agrega más opciones si las necesitas
+seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
+# Según la opción seleccionada, mostramos el contenido correspondiente
+if seleccion == "Ventas":
+mostrar_venta()
+elif seleccion == "Otra opción":
+st.write("Has seleccionado otra opción.") # Aquí podrías agregar el contenido de
+otras opciones
     # Si la sesión está iniciada (el usuario ha iniciado sesión),
     # llamamos a la función que muestra el contenido principal o de ventas.
     mostrar_bienvenido()
