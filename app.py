3582a742d5ace19c3cbd4a30abd2f7293d8d232b
00126222-dev <00126222@uca.edu.sv>
2025-11-20 16:21:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ from modulos.login import login
 # Esta línea verifica si la clave "sesion_iniciada" existe en el estado de la sesión
 # Y también comprueba si su valor es True.
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
-    opciones = ["Ventas", "Otra opción"] # Agrega más opciones si las necesitas
-seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
-# Según la opción seleccionada, mostramos el contenido correspondiente
-if seleccion == "Ventas":
+    opciones = ["Ventas", "Otra opción"] # Agrega más opciones si las necesitas
+    seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
+    # Según la opción seleccionada, mostramos el contenido correspondiente
+    if seleccion == "Ventas":
+        pass # Bloque de código para Ventas (anteriormente vacío)
 
-elif seleccion == "Otra opción":
-st.write("Has seleccionado otra opción.") # Aquí podrías agregar el contenido de
-otras opciones
-    # Si la sesión está iniciada (el usuario ha iniciado sesión),
-    # llamamos a la función que muestra el contenido principal o de ventas.
-    mostrar_bienvenido()
+    elif seleccion == "Otra opción":
+        st.write("Has seleccionado otra opción.") # Aquí podrías agregar el contenido de
+        # otras opciones
+        # Si la sesión está iniciada (el usuario ha iniciado sesión),
+        # llamamos a la función que muestra el contenido principal o de ventas.
+        mostrar_bienvenido()
 else:
-    # Si la sesión no está iniciada o el estado es False,
-    # llamamos a la función que muestra el formulario de inicio de sesión (login).
-    login()
+    # Si la sesión no está iniciada o el estado es False,
+    # llamamos a la función que muestra el formulario de inicio de sesión (login).
+    login()
