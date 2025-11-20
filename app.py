05f22779d31ccde6e3192ca9afcb045242ec806c
00126222-dev <00126222@uca.edu.sv>
2025-11-19 20:32:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,6 @@
-import mysql.connector
-import os
+# app.py
 import streamlit as st
+from modulos.bienvenido import mostrar_bienvenido  # Importamos la función mostrar_venta del módulo venta
 
-# Obtener las credenciales de las variables de entorno (seguridad)
-host = os.getenv("DB_HOST", "bxn6iph4ppjxlv2yiykm-mysql.services.clever-cloud.com")
-user = os.getenv("DB_USER", "ubjkfbdeovoghrwe")
-password = os.getenv("DB_PASSWORD", "eLp25GP5H0OKWtJ8Lggt")
-database = os.getenv("DB_NAME", "bxn6iph4ppjxlv2yiykm")
-
-# Intentar establecer la conexión
-try:
-    conn = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    
-    # Verificar si la conexión fue exitosa
-    if conn.is_connected():
-        st.success("Conexión exitosa a la base de datos MySQL.")
-    else:
-        st.error("No se pudo conectar a la base de datos.")
-        
-except mysql.connector.Error as err:
-    # Mostrar el error si la conexión falla
-    st.error(f"Error al conectar con la base de datos: {err}")
-
-finally:
-    if conn.is_connected():
-        # Cerrar la conexión
-        conn.close()
+# Llamamos a la función mostrar_bienvenido para mostrar el mensaje en la app
+mostrar_bienvenido()
