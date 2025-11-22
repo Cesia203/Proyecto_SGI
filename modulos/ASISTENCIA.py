06d2bc6e669fc65c9815f192e606ab7f38dcb178
Cesia203 <00018422@uca.edu.sv>
2025-11-22 17:25:56 -0600

    Update ASISTENCIA.py

--- modulos/ASISTENCIA.py
+++ modulos/ASISTENCIA.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from modulos.config.conexion import obtener_conexion
 
-def mostrar_ASISTENCIA():
+def ASISTENCIA():
     st.header("📊 Registrar Asistencia")
 
     # Intentar obtener la conexión a la base de datos
