7b4676b8431b7c835ad39a326c1a418c7b7a7b93
Cesia203 <00018422@uca.edu.sv>
2025-11-22 17:30:22 -0600

    Update Prestamo.py

--- modulos/Prestamo.py
+++ modulos/Prestamo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from modulos.config.conexion import obtener_conexion
 
-def mostrar_prestamo():
+def Prestamo():
     st.header("💰 Registrar Préstamo")
 
     # Intentar obtener la conexión a la base de datos
