f014bab2293e36d3dbe9dde5387b282b1c07558f
Cesia203 <00018422@uca.edu.sv>
2025-11-22 17:28:45 -0600

    Update Distrito.py

--- modulos/Distrito.py
+++ modulos/Distrito.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from modulos.config.conexion import obtener_conexion
 
-def mostrar_distrito():
+def Distrito():
     st.header("🗺️ Registrar Distrito")
 
     # Intentar obtener la conexión a la base de datos
