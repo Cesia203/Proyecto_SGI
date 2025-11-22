7569ebfbdfd93ca147db511fcedde59e7d2a6658
Cesia203 <00018422@uca.edu.sv>
2025-11-22 17:30:39 -0600

    Update Promotora.py

--- modulos/Promotora.py
+++ modulos/Promotora.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from modulos.config.conexion import obtener_conexion
 
-def mostrar_promotora():
+def Promotora():
     st.header("👩‍💼 Registrar Promotora")
 
     # Intentar obtener la conexión a la base de datos
