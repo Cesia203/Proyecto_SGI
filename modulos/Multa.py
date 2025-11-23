deb9f511be24e4804d5010cc74904168a19ed4ad
Cesia203 <00018422@uca.edu.sv>
2025-11-23 11:31:13 -0600

    Update Multa.py

--- modulos/Multa.py
+++ modulos/Multa.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from modulos.config.conexion import obtener_conexion
 
-def Multa():
+def mostrar_Multa():
     st.header("💰 Registrar Multa")
 
     # Intentar obtener la conexión a la base de datos
