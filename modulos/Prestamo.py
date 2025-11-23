af02be86900e43fe8bc55ae5991e8ebf96844d49
Cesia203 <00018422@uca.edu.sv>
2025-11-23 11:25:02 -0600

    Update Prestamo.py

--- modulos/Prestamo.py
+++ modulos/Prestamo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from modulos.config.conexion import obtener_conexion
 
-def registrar_prestamo():
+def mostrar_Prestamo():
     st.header("💰 Registrar Préstamo (Simple)")
 
     try:
