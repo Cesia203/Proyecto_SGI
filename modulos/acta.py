75985e7d6c9cff7c8cb1cda7ac44ea3b8625c2b0
Cesia203 <00018422@uca.edu.sv>
2025-11-23 23:36:11 -0600

    Update and rename Acta.py to acta.py

---
+++ modulos/acta.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from datetime import date
+from modulos.config.conexion import obtener_conexion
+
+def registrar_acta():
+    st.header("📄 Registrar Acta")
+
+    try:
+        con = obtener_conexion()
+        cursor = con.cursor()
+
+        with st.form("form_acta"):
+
+            # Campo enum: Tipo de acta
+            tipo = st.selectbox(
+                "Tipo de Acta",
+                ["Ordinaria", "Extraordinaria", "Especial"]
+            )
+
+            fecha = st.date_input("Fecha del Acta", value=date.today())
+
+            contenido = st.text_area("Contenido del Acta")
+
+            id_ciclo = st.number_input("ID Ciclo", min_value=1, step=1)
